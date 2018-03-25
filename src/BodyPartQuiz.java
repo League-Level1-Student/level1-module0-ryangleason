bbdf971d2b51dd2eb94f40fdff1822ca5ebbadfe
andrew983 <league@WorthyMini-5.attlocal.net>
2018-03-25 16:03:22 -0700

    Arnold likes to play drums

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -15,50 +16,84 @@ import javax.swing.JPanel;
 
 public class BodyPartQuiz {
 
-	// 0. You can use the celebrity photos we have placed in the default java package,  
-	// or if you prefer, get celebrity photos from the Internet, place them in the default
+	// 0. You can use the celebrity photos we have placed in the default java
+	// package,
+	// or if you prefer, get celebrity photos from the Internet, place them in the
+	// default
 	// package, and change the names below.
-	
+
 	String firstImage = "src/arnold.jpeg";
 	String secondImage = "src/leonardo.jpeg";
 	String thirdImage = "src/morgan.jpeg";
 	String fourthImage = "src/jack.jpeg";
-	
+
 	JFrame window = new JFrame();
 	JPanel panel = new JPanel();
-
 
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-int score = 0;
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method below
 
 		for (int i = 0; i < 4; i++) {
 			initializeGui();
-			// 4. Ask the user who this person is and store their answer
-			String guess= JOptionPane.showInputDialog("who is this?");
-			
-			// 5. Check their answer. If they guessed correctly:
-			// -- Tell them they are right and increase the score by 1
-
-			// 6. Otherwise:
-			// -- Tell them they are wrong and who the person is
-
-			// 7. Use the showNextImage() method below to get the next image
-			showNextImage();
-		    // 8. Show them their current score
-			
-			// 9. .... repeat for all your images.....
-
-
 		}
+		// 4. Ask the user who this person is and store their answer
+		String guessA = JOptionPane.showInputDialog("who is this?");
+
+		// 5. Check their answer. If they guessed correctly:
+		if (guessA.equalsIgnoreCase("Arnold")) {
+			JOptionPane.showMessageDialog(null, "You are correct");
+			score++;
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Incorrect, it is 'Arnold'");
+		}
+		showNextImage();
+		String guessB = JOptionPane.showInputDialog("who is this?");
+		if (guessB.equalsIgnoreCase("Leonardo")) {
+			JOptionPane.showMessageDialog(null, "You are correct");
+			score++;
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Incorrect, it is 'leonardo'");
+		}
+		showNextImage();
+		String guessC = JOptionPane.showInputDialog("who is this?");
+		if (guessC.equalsIgnoreCase("Morgan")) {
+			JOptionPane.showMessageDialog(null, "You are correct");
+			score++;
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Incorrect, it is 'Morgan'");
+		}
+		showNextImage();
+		String guessD = JOptionPane.showInputDialog("who is this?");
+		if (guessD.equalsIgnoreCase("Black")||guessD.equalsIgnoreCase("jack")) {
+			JOptionPane.showMessageDialog(null, "You are correct");
+			score++;
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Incorrect, it is 'Jack' or ' Black'");
+		}
+		showNextImage();
+		// -- Tell them they are right and increase the score by 1
+
+		// 6. Otherwise:
+		// -- Tell them they are wrong and who the person is
+
+		// 7. Use the showNextImage() method below to get the next image
+
+		// 8. Show them their current score
+		JOptionPane.showMessageDialog(null, "Your score is " + score + " Good job!");
+		// 9. .... repeat for all your images.....
 
 	}
 
 	public void showNextImage() {
 		panel.removeAll();
-		panel.add(getNextImage());		
+		panel.add(getNextImage());
 		window.setVisible(true);
 	}
 
@@ -73,12 +108,12 @@ int score = 0;
 		imageIterator = imageList.iterator();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.add(panel);
-		
-		// 3. Change the size of the window so that you can only see part of the image.		
-		window.setSize(500,500);
-		
+
+		// 3. Change the size of the window so that you can only see part of the image.
+		window.setSize(50, 300);
+
 		showNextImage();
-		
+
 	}
 
 	private JLabel loadImage(String fileName) {

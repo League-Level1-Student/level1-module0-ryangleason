3b0c06e55e8c41d05b694aca4b160be19496a223
ryangleason <league@iMac-2.attlocal.net>
2018-04-08 15:57:09 -0700

    Go away & Mac vs. PC squabbles

--- src/DuelingButtons.java
+++ src/DuelingButtons.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -12,60 +13,75 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class DuelingButtons implements ActionListener {
-	
+
 	public static void main(String[] args) {
 		new DuelingButtons().createUI();
 	}
 
 	JButton leftButton = new JButton();
 	JButton rightButton = new JButton();
-	
-	Dimension BIG = new Dimension(400,400);
-	Dimension SMALL = new Dimension(200,200);
-	
+
+	Dimension BIG = new Dimension(400, 400);
+	Dimension SMALL = new Dimension(200, 200);
+
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 
 	private void createUI() {
 		// 1. Add the panel to the frame
-
+		frame.add(panel);
 		// 2. Make the frame visible
-
+		frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
+
+		leftButton.setText("MAC");
+		panel.add(leftButton);
+		rightButton.setText("PC");
+		panel.add(rightButton);
+
+		// frame.setSize(600, 600);
 
 		// 4. Set the text of the rightButton to "Click me!"
 
 		// 5. Add an action listener to the leftButton
-
+		leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
-		// 7. Add the leftButton to the panel
-
-		// 8. Add the rightButton to the panel
+		rightButton.addActionListener(this);
 
 		// 9. Pack the frame
-
+		frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
+		frame.setTitle("Demanding Buttons");
 
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-		
-		
-		/* If the buttonPressed was the leftButton....*/
-			// Set the text of the rightButton to "No, click Me!"
+
+		/* If the buttonPressed was the leftButton.... */
+		// Set the text of the rightButton to "No, click Me!"
+		if (buttonPressed == leftButton) {
+			rightButton.setText("I can play games");
+			rightButton.setPreferredSize(BIG);
+			leftButton.setPreferredSize(SMALL);
+			rightButton.setSize(BIG);
+		}
+			if (buttonPressed == rightButton) {
+				System.out.println("Right");
+				leftButton.setText("I don't get virusus");
+				rightButton.setPreferredSize(SMALL);
+				leftButton.setPreferredSize(BIG);
+				leftButton.setSize(BIG);
+			}
 			// Set the PREFERRED size of the rightButton to BIG
+
 			// Set the text of the leftButton to "Click Me!"
 			// Set the PREFERRED size of the leftButton to SMALL
-		
-		
-		/* If the buttonPressed was the rightButton, do the opposite. */
-		
 
-		frame.pack();
+			/* If the buttonPressed was the rightButton, do the opposite. */
+
+			frame.pack();
+		}
 	}
-}
-
 
